497fad9b0d083194cef0701aa17f4d9f091b8627
complooea <complooea@gmail.com>
2025-09-11 17:03:37 +0800

    Fix package declaration in setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(name='Chargym_Charging_Station',
       version='0.0.1',
+      packages=['Chargym_Charging_Station'],
       install_requires=['gym']#And any other dependencies required
 )
