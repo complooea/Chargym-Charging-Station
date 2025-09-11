9ada462a7b8529e278a303546a687571ac597801
complooea <complooea@gmail.com>
2025-09-11 17:42:31 +0800

    Remove install_requires in setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,4 @@ from setuptools import setup
 setup(name='Chargym_Charging_Station',
       version='0.0.1',
       packages=['Chargym_Charging_Station'],
-      install_requires=['gym']#And any other dependencies required
 )
