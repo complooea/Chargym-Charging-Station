2195955127c91cb7c120f4d5b74b7c89830deb64
complooea <complooea@gmail.com>
2025-09-13 16:24:59 +0800

    Fix setup.py to ensure scipy dependency is included in install_requires

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='Chargym_Charging_Station',
       install_requires=[
             "setuptools==59.5.0",
             "wheel>=0.36,<0.40",
-            "stable-baselines3[extra]==1.4.0"
+            "stable-baselines3[extra]==1.4.0",
+            "scipy==1.7.3",
       ],
 )
