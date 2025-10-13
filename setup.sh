5f240068443890afa18de493ab5e622499d0962f
complooea <complooea@gmail.com>
2025-10-13 12:53:48 +0800

    Update setup script to source .bashrc after modifications

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
+sed -i 's|cd /data/coding|# cd /data/coding|' ~/.bashrc
+
 git config --global user.name "complooea"
 git config --global user.email "complooea@gmail.com"
 
 apt-get update
 apt-get install zlib1g-dev
+
+source ~/.bashrc
 
 pip install -e .
